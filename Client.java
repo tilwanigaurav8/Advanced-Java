98e6e4d03bccdd711cebadb8dec6abd408c58e2e
tilwanigaurav8 <tilwanigaurav8@gmail.com>
2023-05-26 23:36:04 +0530

    26/05/22

--- Client.java
+++ Client.java
@@ -2,9 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.io.Serializable;
 import java.net.Socket;
 
-public class Client {
+public class Client implements Serializable{
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 1234);
